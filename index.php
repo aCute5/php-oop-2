b50a5643a1cde27fe5d79b956becac3f7b5a34ab
aCute5 <costantini95dario@gmail.com>
2023-01-10 17:23:01 +0100

    include not working

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 // BONUS:
 // Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da
 // maggio ad agosto). -->
+include_once __DIR__ . '/user.php';
+include __DIR__ . '/registered.php';
+include __DIR__
 
-
-?>
+    ?>
--- not registered.php
+++ not registered.php
@@ -1,1 +1,15 @@
 <?php
+
+class Notregistered extends User
+{
+
+    public $discount;
+
+    public function setDiscount($discount)
+    {
+        $this->discount = 0;
+        return $this;
+    }
+}
+
+$registered->setDiscount(0);
